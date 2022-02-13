c1952f74cfa8e1241233e977a1498050d484c28e
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-02-13 18:13:33 -0500

    Agregar archivos de proyecto.

---
+++ WebAppBlazorCRUD/Data/Cerveza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace WebAppBlazorCRUD.Data
+{
+    public class Cerveza
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El Nombre es Obligatorio")]
+        public string Nombre { get; set; }
+        [Required]
+        public string Marca { get; set; }
+    }
+}
